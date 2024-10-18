5d337b72800b239df9332ce548a8e16a43bbcf1c
Alejandra Sanabria <128005730+Aleja-xd@users.noreply.github.com>
2024-10-17 21:20:37 -0500

    Cambios en Usuario

--- Entrega 2/Proyecto1/src/proyecto/Usuario.java
+++ Entrega 2/Proyecto1/src/proyecto/Usuario.java
@@ -1,5 +1,10 @@
 package proyecto;
+import java.util.Date;
 
 public class Usuario {
-
+    private int id;
+    private String nombre;
+    private String correo;
+    private String contrasena;
+    private Date fechaRegistro;
 }
