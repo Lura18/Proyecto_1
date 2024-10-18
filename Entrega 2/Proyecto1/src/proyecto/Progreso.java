7b4eb4278c9e1882a6b7b82d5a16f88593a5dd7c
Alejandra Sanabria <128005730+Aleja-xd@users.noreply.github.com>
2024-10-17 21:29:27 -0500

    Update Progreso.java

--- Entrega 2/Proyecto1/src/proyecto/Progreso.java
+++ Entrega 2/Proyecto1/src/proyecto/Progreso.java
@@ -1,5 +1,10 @@
 package proyecto;
+import java.util.Date;
 
 public class Progreso {
-
+    private Date fechaInicioPath;
+    private Date fechaFinPath;
+    private float porcentajePath;
+    private float tasaExito;
+    private float tasaFracaso;
 }
