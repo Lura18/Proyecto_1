73b438ce2bcfe75d5812c4fa2321bae380e4631d
anaismariacr <am.castro23@uniandes.edu.co>
2024-11-14 19:32:01 -0500

    prueba junit

---
+++ Entrega 2/Proyecto 2/src/testProyecto/TestProfesor.java
@@ -0,0 +1,13 @@
+package testProyecto;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestProfesor {
+
+	
+	@Test
+	public void verLearningPathTest(){
+		
+		
+	}
+}
