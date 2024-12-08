7a4fd6ab9543bfae7e0f40ec66ef07ee9272d194
anaismariacr <am.castro23@uniandes.edu.co>
2024-12-08 13:08:45 -0500

    paquetes recomendaciones

---
+++ Entrega 2/Proyecto 2/src/actividadesYRecomendaciones/PanelActividadesCompletadas.java
@@ -0,0 +1,5 @@
+package actividadesYRecomendaciones;
+
+public class PanelActividadesCompletadas {
+
+}
---
+++ Entrega 2/Proyecto 2/src/actividadesYRecomendaciones/PanelNavegar.java
@@ -0,0 +1,20 @@
+package actividadesYRecomendaciones;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+
+public class PanelNavegar extends JPanel implements ActionListener{
+
+	private static final String regresar = "regresar";
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
